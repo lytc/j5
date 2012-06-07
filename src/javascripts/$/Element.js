3868f45122b54e8fe889e4a24d16cfc3025feb31
Ly Tran <prtran@gmail.com>
2012-06-07 15:29:02 +0700

    doc

--- src/javascripts/$/Element.js
+++ src/javascripts/$/Element.js
@@ -374,6 +374,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method toggleClasses
+     * @param String classes
+     * @return $.Element
+     */
     ,toggleClasses: function(classes) {
         var classList = this.getClasses();
         if (!classList.length) {
@@ -389,6 +394,12 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method switchClasses
+     * @param Boolean bool
+     * @param String classes
+     * @return $.Element
+     */
     ,switchClasses: function(bool, classes) {
         if (bool) {
             return this.addClasses(classes);
@@ -396,16 +407,31 @@ $.Observable.extend('$.Element element', {
         return this.removeClasses(classes);
     }
 
+    /**
+     * @method radioClasses
+     * @param String classes
+     * @return $.Element
+     */
     ,radioClasses: function(classes) {
         this.getParent().children().removeClasses(classes);
         return this.addClasses(classes);
     }
 
+    /**
+     * @method deRadioClasses
+     * @param String classes
+     * @return $.Element
+     */
     ,deRadioClasses: function(classes) {
         this.getParent().children().addClasses(classes);
         return this.removeClasses(classes);
     }
 
+    /**
+     * @method setClickRadioClasses
+     * @param String classes
+     * @return $.Element
+     */
     ,setClickRadioClasses: function(classes) {
         this.on('click', function() {
             this.radioClasses(classes);
@@ -413,6 +439,12 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method setStyles
+     * @param String|Object name
+     * @param Mixed [value]
+     * @return $.Element
+     */
     ,setStyles: function(name, value) {
         if ('string' != typeof name) {
             $.each(name, function(v, k) {
@@ -426,6 +458,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method getStyle
+     * @param String name
+     * @return String
+     */
     ,getStyle: function(name) {
         name = $.getCssProperty(name);
         var result = this.dom.style[name];
@@ -436,6 +473,10 @@ $.Observable.extend('$.Element element', {
         return result;
     }
 
+    /**
+     * @method removeStyles
+     * @param String... name
+     */
     ,removeStyles: function() {
         var name;
 
@@ -447,6 +488,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @mehtod setWidth
+     * @param Number|String width
+     * @return $.Element
+     */
     ,setWidth: function(width) {
         if ('number' == typeof width) {
             width += 'px';
@@ -454,10 +500,19 @@ $.Observable.extend('$.Element element', {
         return this.setStyles('width', width);
     }
 
+    /**
+     * @method getWidth
+     * @return Number
+     */
     ,getWidth: function() {
         return this.dom.offsetWidth;
     }
 
+    /**
+     * @method setHeight
+     * @param Number|String height
+     * @return $.Element
+     */
     ,setHeight: function(height) {
         if ('number' == typeof height) {
             height += 'px';
@@ -465,19 +520,38 @@ $.Observable.extend('$.Element element', {
         return this.setStyles('height', height);
     }
 
+    /**
+     * @method getHeight
+     * @return Number
+     */
     ,getHeight: function() {
         return this.dom.offsetHeight;
     }
 
+    /**
+     * @method setSize
+     * @param Number|String width
+     * @param Number|String [height]
+     * @return $.Element
+     */
     ,setSize: function(width, height) {
         ('undefined' == height) || (height = width);
         return this.setWidth(width).setHeight(height);
     }
 
+    /**
+     * @method getOffsetParent
+     * @return Element
+     */
     ,getOffsetParent: function() {
         return this.dom.offsetParent;
     }
 
+    /**
+     * @method setTop
+     * @param Number top
+     * @return $.Element
+     */
     ,setTop: function(top) {
         if ('number' == typeof top) {
             top += 'px';
@@ -485,10 +559,19 @@ $.Observable.extend('$.Element element', {
         return this.setStyles('top', top);
     }
 
+    /**
+     * @method getTop
+     * @return Number
+     */
     ,getTop: function() {
         return this.dom.offsetTop;
     }
 
+    /**
+     * @method setRight
+     * @param Number right
+     * @return $.Element
+     */
     ,setRight: function(right) {
         if ('number' == typeof right) {
             right += 'px';
@@ -496,12 +579,21 @@ $.Observable.extend('$.Element element', {
         return this.setStyles('right', right);
     }
 
+    /**
+     * @method getRigth
+     * @return Number
+     */
     ,getRight: function() {
         var right = this.getStyle('right');
         right = parseInt(right);
         return isNaN(right)? undefined : right;
     }
 
+    /**
+     * @method setLeft
+     * @param Number left
+     * @return $.Element
+     */
     ,setLeft: function(left) {
         if ('number' == typeof left) {
             left += 'px';
@@ -509,24 +601,18 @@ $.Observable.extend('$.Element element', {
         return this.setStyles('left', left);
     }
 
-    ,getLeft: function(left) {
+    /**
+     * @method getLeft
+     * @return Number
+     */
+    ,getLeft: function() {
         return this.dom.offsetLeft;
     }
 
-    /*,getOffset: function() {
-        var offset, x = this.dom.offsetLeft, y = this.dom.offsetTop;
-        if (offset = this.dom.offsetParent) {
-            do {
-                x += offset.offsetLeft;
-                y += offset.offsetTop;
-            } while (offset = offset.offsetParent);
-        }
-        return {
-            x: x
-            ,y: y
-        };
-    }
-    */
+    /**
+     * @method getOffset
+     * @return Object
+     */
     ,getOffset: function() {
         return {
             x: this.dom.offsetLeft
@@ -534,6 +620,10 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method getPageXY
+     * @return Object
+     */
     ,getPageXY: function() {
         var xy = this.getOffset()
             ,offsetParent;
@@ -547,20 +637,38 @@ $.Observable.extend('$.Element element', {
         return xy;
     }
 
+    /**
+     * @method setParent
+     * @param Object|Element parent
+     * @return $.Element
+     */
     ,setParent: function(parent) {
         parent = $.Element.get(parent);
         parent.append(this);
         return parent;
     }
 
+    /**
+     * @method getParent
+     * @return $.Element
+     */
     ,getParent: function() {
         return $.Element.get(this.dom.parentElement);
     }
 
+    /**
+     * @method count
+     * @return Number
+     */
     ,count: function() {
         return this.dom.childElementCount;
     }
 
+    /**
+     * @method setIndex
+     * @param Number index
+     * @return $.Element
+     */
     ,setIndex: function(index) {
         var targetEl = this.getParent().at(index);
         if (!targetEl) {
@@ -570,37 +678,72 @@ $.Observable.extend('$.Element element', {
         return this.insertBefore(targetEl);
     }
 
+    /**
+     * @method getIndex
+     * @return Number
+     */
     ,getIndex: function() {
         return [].indexOf.call(this.dom.parentElement.children, this.dom);
     }
 
+    /**
+     * @method up
+     * @param Number [offset]
+     * @return $.Element
+     */
     ,up: function(offset) {
         offset || (offset = 1);
         return this.setIndex(this.getIndex() - offset);
     }
 
+    /**
+     * @method down
+     * @param Number [offset]
+     * @return $.Element
+     */
     ,down: function(offset) {
         offset || (offset = 1);
         return this.up(-offset - 1);
     }
 
-    ,first: function(query, uniq) {
-        return this.at(0, query, uniq);
+    /**
+     * @method first
+     * @param String [query]
+     * @return $.Element
+     */
+    ,first: function(query) {
+        return this.at(0, query);
     }
 
-    ,last: function(query, uniq) {
-        return this.at(this.count() - 1, query, uniq);
+    /**
+     * @method last
+     * @param String [query]
+     * @return $.Element
+     */
+    ,last: function(query) {
+        return this.at(this.count() - 1, query);
     }
 
-    ,at: function(at, query, uniq) {
+    /**
+     * @method at
+     * @param Number at
+     * @param String ]query]
+     * @return $.Element
+     */
+    ,at: function(at, query) {
         query = '>' + (query || '*') + ':nth-child(' + (at + 1) + ')';
 
         var dom = $.Dom.query(query, this.dom);
         if (dom) {
-            return $.Element.get(dom, this, uniq);
+            return $.Element.get(dom, this);
         }
     }
 
+    /**
+     * @method append
+     * @param Element|Array els
+     * @return $.Element
+     */
     ,append: function(els) {
         if (els instanceof Array) {
             return $.each(els, function(el) {
@@ -614,6 +757,12 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method insert
+     * @param Number at
+     * @param Object|Element el
+     * @return $.Element
+     */
     ,insert: function(at, el) {
         var atDom = this.dom.children[at];
         if (atDom) {
@@ -625,18 +774,32 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method insertBefore
+     * @param Object|$.Element
+     * @return $.Element
+     */
     ,insertBefore: function(el) {
         el = $.Element.get(el);
         el.dom.parentElement.insertBefore(this.dom, el.dom);
         return this;
     }
 
+    /**
+     * @method insertAfter
+     * @param Object|$.Element
+     * @return $.Element
+     */
     ,insertAfter: function(el) {
         el = $.Element.get(el);
         el.dom.parentElement.insertBefore(this.dom, el.dom.nextElementSibling);
         return this;
     }
 
+    /**
+     * @method getNext
+     * @return $.Element
+     */
     ,getNext: function() {
         var dom = this.dom.nextElementSibling;
         if (dom) {
@@ -644,18 +807,31 @@ $.Observable.extend('$.Element element', {
         }
     }
 
-    ,getPrev: function(uniq) {
+    /**
+     * @method getPrev
+     * @return $.Element
+     */
+    ,getPrev: function() {
         var dom = this.dom.previousElementSibling;
         if (dom) {
-            return $.Element.get(dom, this, uniq);
+            return $.Element.get(dom, this);
         }
     }
 
+    /**
+     * @method children
+     * @return $.Elements
+     */
     ,children: function() {
         var els = new $.Elements(this.dom.children);
         return els;
     }
 
+    /**
+     * @method onRender
+     * @param Function callback
+     * @return $.Element
+     */
     ,onRender: function(callback) {
         if (this.isRendered()) {
             callback();
@@ -664,8 +840,15 @@ $.Observable.extend('$.Element element', {
         this.on('render', function() {
             callback();
         })
+
+        return this;
     }
 
+    /**
+     * @method appendTo
+     * @param String|Element|$.Element el
+     * @return $.Element
+     */
     ,appendTo: function(el) {
         el = $.Element.get(el);
         el.append(this);
@@ -674,10 +857,20 @@ $.Observable.extend('$.Element element', {
         return el;
     }
 
+    /**
+     * @method setAppendTo
+     * @param String|Element|$.Element
+     * @return $.Element
+     */
     ,setAppendTo: function(el) {
         return this.appendTo(el);
     }
 
+    /**
+     * @method wrap
+     * @param Object|Element|$.Element
+     * @return $.Element
+     */
     ,wrap: function(el) {
         el || (el = '<div>');
         el = $.Element.get(el);
@@ -691,6 +884,10 @@ $.Observable.extend('$.Element element', {
         return el;
     }
 
+    /**
+     * @method unwrap
+     * @return $.Element
+     */
     ,unwrap: function() {
         if (this.dom.$wrapEl) {
             this.before(this.dom.$wrapEl);
@@ -700,16 +897,30 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method setHtml
+     * @param String html
+     * @return $.Element
+     */
     ,setHtml: function(html) {
         this.dom.innerHTML = html;
         this.trigger('change change:html', this);
         return this;
     }
 
+    /**
+     * @method getHtml
+     * @return String
+     */
     ,getHtml: function() {
         return this.dom.innerHTML;
     }
 
+    /**
+     * @method query
+     * @param String query
+     * @return $.Element
+     */
     ,query: function(query) {
         var dom = $.Dom.query(query, this.dom);
         if (dom) {
@@ -717,6 +928,11 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method queryAll
+     * @param String query
+     * @return $.Elements
+     */
     ,queryAll: function(query) {
         var doms = $.Dom.queryAll(query, this.dom);
         if (doms) {
@@ -724,10 +940,21 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method contains
+     * @param Element|$.Element
+     * @return Boolean
+     */
     ,contains: function(el) {
-        return this.dom.contains(el.dom);
+        var dom = el instanceof $.Element? el.dom : el;
+        return this.dom.contains(dom);
     }
 
+    /**
+     * @method findAncestor
+     * @param String query
+     * @return $.Element
+     */
     ,findAncestor: function(query) {
         query = '> ' + query;
 
@@ -753,10 +980,19 @@ $.Observable.extend('$.Element element', {
         }
     }
 
+    /**
+     * @method isVisible
+     * @return Boolean
+     */
     ,isVisible: function() {
         return !('none' == this.getStyle('display') || this.hasClasses('x-hidden'));
     }
 
+    /**
+     * @method hide
+     * @param Number [defer]
+     * @return $.Element
+     */
     ,hide: function(defer) {
         !this._showHideDefer || (this._showHideDefer.cancel());
 
@@ -774,6 +1010,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method show
+     * @param Number [defer]
+     * @return $.Element
+     */
     ,show: function(defer) {
         !this._showHideDefer || (this._showHideDefer.cancel());
 
@@ -791,30 +1032,63 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method setHidden
+     * @param Boolean bool
+     * @param Number [defer]
+     * @return $.Element
+     */
     ,setHidden: function(bool, defer) {
         return this[bool? 'show' : 'hide'](defer);
     }
 
+    /**
+     * @method toggleDisplay
+     * @param Number [defer]
+     * @return $.Element
+     */
     ,toggleDisplay: function(defer) {
         return this.setHidden(!this.isVisible(), defer);
     }
 
-    ,collapse: function(size, side) {
+    /**
+     * @method collapse
+     * @return $.Element
+     */
+    ,collapse: function() {
         return this.addClasses('x-collapsed');
     }
 
+    /**
+     * @method expand
+     * @return $.Element
+     */
     ,expand: function() {
         return this.removeClasses('x-collapsed');
     }
 
+    /**
+     * @method setCollapsed
+     * @param Boolean bool
+     * @return $.Element
+     */
     ,setCollapsed: function(bool) {
         return this[bool? 'collapse' : 'expand']();
     }
 
+    /**
+     * @method toggleCollapse
+     * @return $.Element
+     */
     ,toggleCollapse: function() {
         this.toggleClasses('x-collapsed');
     }
 
+    /**
+     * @method mask
+     * @param String html
+     * @return $.Element
+     */
     ,mask: function(html) {
         html || (html = '');
         if (!this._maskEl) {
@@ -848,6 +1122,10 @@ $.Observable.extend('$.Element element', {
         return this._maskEl;
     }
 
+    /**
+     * @method unmask
+     * @return $.Element
+     */
     ,unmask: function() {
         if (this._maskEl) {
             this._maskEl.destroy();
@@ -856,13 +1134,24 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method setDraggable
+     * @param Object [options]
+     * @return $.Element
+     */
     ,setDraggable: function(options) {
         if (!this.drag) {
             this.drag = new $.Drag(this);
         }
         this.drag.applyOptions(options);
+        return this;
     }
 
+    /**
+     * @method setKeyListener
+     * @param Object options
+     * @return $.Element
+     */
     ,setKeyListener: function(options) {
         if (!this.keyListener) {
             this.keyListener = new $.KeyListener(this, options);
@@ -870,6 +1159,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method setDefaults
+     * @param Object options
+     * @return $.Element
+     */
     ,setDefaults: function(options) {
         this.on('append', function(el) {
             if (el instanceof $.field.Field) {
@@ -879,6 +1173,11 @@ $.Observable.extend('$.Element element', {
         return this;
     }
 
+    /**
+     * @method clone
+     * @param Boolean deep
+     * @return $.Element
+     */
     ,clone: function(deep) {
         var dom = this.dom.cloneNode(!!deep);
         var el = new $.Element(dom);
@@ -893,10 +1192,18 @@ $.Observable.extend('$.Element element', {
         return el;
     }
 
+    /**
+     * @method isRendered
+     * @return Boolean
+     */
     ,isRendered: function() {
         return document.body.contains(this.dom);
     }
 
+    /**
+     * @method fx
+     * @return $.Animation
+     */
     ,fx: function(options) {
         if (!this._animation) {
             this._animation = new $.Animation(this);
@@ -905,16 +1212,29 @@ $.Observable.extend('$.Element element', {
         return this._animation;
     }
 
+    /**
+     * @method empty
+     * @return $.Element
+     */
     ,empty: function() {
-        this.setHtml('');
+        return this.setHtml('');
     }
 
+    /**
+     * @method destroy
+     */
     ,destroy: function() {
         this.dom.parentNode.removeChild(this.dom);
     }
 });
 
 $.extend($.Element, {
+    /**
+     * @static
+     * @method getDom
+     * @param Mixed el
+     * @return Element
+     */
     getDom: function(el) {
         if (el instanceof $.Element) {
             return el.dom;
@@ -925,6 +1245,13 @@ $.extend($.Element, {
         }
     }
 
+    /**
+     * @static
+     * @method get
+     * @param Mixed object
+     * @param String|Element|$.Element
+     * @return $.Element
+     */
     ,get: function(obj, root) {
         if (obj instanceof $.Element) {
             return obj;
